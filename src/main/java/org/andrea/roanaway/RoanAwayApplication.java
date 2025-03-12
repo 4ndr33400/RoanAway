1ee2f1df0a1839be01179a88545cf787c0beab47
4ndr33400 <andrmontal@gmail.com>
2025-03-12 20:37:15 +0100

    commit inicial

---
+++ src/main/java/org/andrea/roanaway/RoanAwayApplication.java
@@ -0,0 +1,13 @@
+package org.andrea.roanaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoanAwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoanAwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/andrea/roanaway/RoanAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andrea.roanaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoanAwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
